ba8f7e88496b6b23113a5623071a98761dd3e55e
roycekumar <royce3062@gmail.com>
2020-11-20 17:27:20 +0530

    Added via Visual Studio Code

--- main.js
+++ main.js
@@ -1,7 +1,28 @@
 
+
 function setup() {
   canvas = createCanvas(380, 380);
   canvas.center();
-
+  video = createCapture(VIDEO);
+  video.size(380,380);
+  video.hide();
 }
 
+function modelLoaded() {
+  console.log("Model Loaded!")
+  status = true;
+}
+
+function start()
+{
+  objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+  object_name = document.getElementById("object_name").value;
+  document.getElementById("status").innerHTML = "Status : Detecting Objects";
+  
+}
+
+
+function draw() {
+  image(video, 0, 0, 380, 380);
+  
+}
